b269773c2d85ffa0ded81108be4ecae734724ef6
runningtalus <dave@davemay.org>
2014-04-10 10:28:09 -0600

    Additional examples

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ In-class exercise
 2) On page load, dynamically add a UL
 3) Create a JS Array that contains a list of string s that are your favorite movies.
 4) Using DOM creation, append an LI for each title in the array to the dynamically created UL
+
+
+FIRST use of...
+	BEFORE
+	AFTER
+	EMPTY
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // $('#test');
-// $('#test').append('APPEND makes content render AFTER element ID or CLASS specified');
+// $('#test').append('APPEND makes content render at the bottom of the element ID or CLASS specified');
 
-// $('#test').prepend('PREPEND makes content render BEFORE element ID or CLASS specified');
+// $('#test').prepend('PREPEND makes content render at the top of the element ID or CLASS specified');
 
 // $('#test');
 // $('#test').append('<li>This is from jQuery-create items in the DOM from jQuery!!!!<li>');
+
+//Exercise
 
 $(document).on('ready', function() {
 	$('body').append('<ul id="test"></ul>');
@@ -17,4 +19,46 @@ $(document).on('ready', function() {
 		$('#test').append('<li>' + currentGame + '</li>');
 		console.log('<li>' + currentGame + '</li>');
 	};
+
+
+//using CLONE & storing it in a variable
+
+var newGameUL=$('#test').clone();
+newGameUL.append('<li>This is a clone</li>');
+newGameUL.css('backgroundColor', '#F00');
+
+$('body').append(newGameUL);
+
+
+//FIRST USE OF .empty
+//$('ul').empty();
+
+
+// $('li:first');
+// //CDT <li>Braid</li>
+// $('li:first').remove();
+//CDT removes <li>Braid<li>
+
+//using the power of $()
+//storing jQuery as a variable
+//assigning it CSS rules
+//dynamically injecting it into the DOM
+
+var newElement = $('<h1>Here is an H1</h1>');
+newElement.css('color', '#F00');
+$('body').prepend(newElement);
+
+
+//Alternative shorthands for the "on Ready, document ready, methods"
+
+// $(document).on('ready', function(){});
+// $(document).ready(function(){});
+// $(function (){});
+
+
+
+
+
+
 });
+//
