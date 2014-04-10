8d1f2c1586263d9357785010e52fb930fe2e13c8
runningtalus <dave@davemay.org>
2014-04-10 09:54:32 -0600

    Added examples

---
+++ README.md
@@ -0,0 +1,12 @@
+Document Object Model (DOM) Events & Creation
+
+
+FIRST use of...
+	APPEND- places items after element
+	PREPEND- 
+
+In-class exercise
+1) Create an HTML page
+2) On page load, dynamically add a UL
+3) Create a JS Array that contains a list of string s that are your favorite movies.
+4) Using DOM creation, append an LI for each title in the array to the dynamically created UL
--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// $('#test');
+// $('#test').append('APPEND makes content render AFTER element ID or CLASS specified');
+
+// $('#test').prepend('PREPEND makes content render BEFORE element ID or CLASS specified');
+
+// $('#test');
+// $('#test').append('<li>This is from jQuery-create items in the DOM from jQuery!!!!<li>');
+
 $(document).on('ready', function() {
-  
+	$('body').append('<ul id="test"></ul>');
+	var games=['Braid', 'Swapper', 'Project Zomboid'];
+	console.log(games);
+	
+
+	for (var i=0; i<games.length; i++){
+		var currentGame= games[i];
+		$('#test').append('<li>' + currentGame + '</li>');
+		console.log('<li>' + currentGame + '</li>');
+	};
 });
